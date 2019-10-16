260db48e11aa3eca07ecf8230daf5028bdd13236
Jules Sayer <jules.sayer@protonmail.com>
2019-10-16 20:13:59 +0200

    English

--- src/Cavalier.java
+++ src/Cavalier.java
@@ -1,60 +1,28 @@
 import java.util.Arrays;
-
 /**
- * Cette classe permet d'afficher l'échiquier
- * pour UNE solution. L'affichage de cette
- * solution se fait en indiquant le numéro de
- * l'étape pour chaque case de l'échiquier.
- *
- * Comme indiqué dans le sujet il est nécessaire
- * de passer en argument X0, Y0 et N.
- *
  * @author Jules Sayer
  * @version 1.0
  */
 public class Cavalier {
-    /**
-     * Echiquier qui enregistre les solutions
-     */
-    private static int[][] echiquier;
-
-    /**
-     * Taille de l'échiquier
-     */
-    private static int taille;
-
-    /**
-     * Mouvements possible d'un cavalier en X
-     */
+    private static int[][] chessboard;
+    private static int size;
     private static int[] mvtsX = {-2, -2, -1, -1, 1, 1, 2, 2};
-
-    /**
-     * Mouvements possible d'un cavalier en Y
-     */
     private static int[] mvtsY = {-1, 1, -2, 2, -2, 2, -1, 1};
 
-    /**
-     * Fonction récursive permettant de trouver UNE solution
-     *
-     * @param x position du cavalier en X
-     * @param y position du cavalier en Y
-     * @param n numéro de l'étape
-     * @return boolean Si la fonction a trouvé une solution complète ou non
-     */
-    private static boolean rechercheSolution(int x, int y, int n) {
+    private static boolean findSolutions(int x, int y, int n) {
         boolean res = false;
-        if (n > (taille * taille)) {
+        if (n > (size * size)) {
             res = true;
         } else {
             for (int i = 0; i < 8; i++) {
                 int nextX = x + mvtsX[i];
                 int nextY = y + mvtsY[i];
-                if (nextX >= 0 && nextX < taille && nextY >= 0 && nextY < taille && echiquier[nextX][nextY] == 0) {
-                    echiquier[nextX][nextY] = n;
-                    if (rechercheSolution(nextX, nextY, n + 1)) {
+                if (nextX >= 0 && nextX < size && nextY >= 0 && nextY < size && chessboard[nextX][nextY] == 0) {
+                    chessboard[nextX][nextY] = n;
+                    if (findSolutions(nextX, nextY, n + 1)) {
                         res = true;
                     } else {
-                        echiquier[nextX][nextY] = 0;
+                        chessboard[nextX][nextY] = 0;
                     }
                 }
             }
@@ -63,31 +31,28 @@ public class Cavalier {
     }
 
     public static void main(String[] args) throws Exception {
-        /* Test arguments */
         if (args.length != 3) {
-            throw new Exception("Comme indiqué dans la doc, il faut 3 arguments afin de lancer le programme.");
+            throw new Exception("You need to use args like: StartPosX StartPosY Size");
         }
 
-        /* Initialisation de l'échiquier et placement du pion. */
-        taille = Integer.parseInt(args[2]);
+        size = Integer.parseInt(args[2]);
         int posXD = Integer.parseInt(args[0]);
         int posYD = Integer.parseInt(args[1]);
-        echiquier = new int[taille][taille];
-        for (int[] row : echiquier) {
+        chessboard = new int[size][size];
+        for (int[] row : chessboard) {
             Arrays.fill(row, 0);
         }
-        echiquier[posXD][posYD] = 1;
+        chessboard[posXD][posYD] = 1;
 
-        /* Appel de la fonction récursive */
-        if (rechercheSolution(posXD, posYD, 2)) {
-            for (int[] ligne : echiquier) {
+        if (findSolutions(posXD, posYD, 2)) {
+            for (int[] ligne : chessboard) {
                 for (int n : ligne) {
                     System.out.print("\t" + n);
                 }
                 System.out.println();
             }
         } else {
-            System.err.println("Il n'existe aucune solution.");
+            System.err.println("There is no solutions.");
         }
     }
 }
--- src/CavalierALL.java
+++ src/CavalierALL.java
@@ -1,84 +1,46 @@
 import java.util.Arrays;
-
 /**
- * Cette classe permet d'afficher le nombre
- * de solutions possible pour un X0, Y0 et N
- * donnés.
- * Comme indiqué dans le sujet il est nécessaire
- * de passer en argument X0, Y0 et N.
- *
  * @author Jules Sayer
  * @version 1.0
  */
 public class CavalierALL {
-    /**
-     * Echiquier qui enregistre les solutions
-     */
-    private static int[][] echiquier;
-
-    /**
-     * Taille de l'échiquier
-     */
-    private static int taille;
-
-    /**
-     * Mouvements possible d'un cavalier en X
-     */
+    private static int[][] chessboard;
+    private static int size;
     private static int[] mvtsX = {-2, -2, -1, -1, 1, 1, 2, 2};
-
-    /**
-     * Mouvements possible d'un cavalier en Y
-     */
     private static int[] mvtsY = {-1, 1, -2, 2, -2, 2, -1, 1};
-
-    /**
-     * Nombre de solutions incrémenté lors de l'appel
-     * de la fonction
-     */
     private static int nbSolutions = 0;
 
-    /**
-     * Fonction récursive permettant de trouver toutes les solutions
-     * (elle incrémente le nombre de solution trouvées et ne les affiche pas)
-     *
-     * @param x position du cavalier en X
-     * @param y position du cavalier en Y
-     * @param n numéro de l'étape
-     */
-    private static void rechercheSolution(int x, int y, int n) {
-        if (n > taille * taille) {
+    private static void findSolutions(int x, int y, int n) {
+        if (n > size * size) {
             nbSolutions++;
         } else {
             for (int i = 0; i < 8; i++) {
                 int nextX = x + mvtsX[i];
                 int nextY = y + mvtsY[i];
-                if (nextX >= 0 && nextX < taille && nextY >= 0 && nextY < taille && echiquier[nextX][nextY] == 0) {
-                    echiquier[nextX][nextY] = n;
-                    rechercheSolution(nextX, nextY, n + 1);
-                    echiquier[nextX][nextY] = 0; // Retour en arrière
+                if (nextX >= 0 && nextX < size && nextY >= 0 && nextY < size && chessboard[nextX][nextY] == 0) {
+                    chessboard[nextX][nextY] = n;
+                    findSolutions(nextX, nextY, n + 1);
+                    chessboard[nextX][nextY] = 0; // Retour en arrière
                 }
             }
         }
     }
 
     public static void main(String[] args) throws Exception {
-        /* Test arguments */
         if (args.length != 3) {
-            throw new Exception("Comme indiqué dans la doc, il faut 3 arguments afin de lancer le programme.");
+            throw new Exception("You need to use args like: StartPosX StartPosY Size");
         }
 
-        /* Initialisation de l'échiquier et placement du pion. */
-        taille = Integer.parseInt(args[2]);
+        size = Integer.parseInt(args[2]);
         int posXD = Integer.parseInt(args[0]);
         int posYD = Integer.parseInt(args[1]);
-        echiquier = new int[taille][taille];
-        for (int[] row : echiquier) {
+        chessboard = new int[size][size];
+        for (int[] row : chessboard) {
             Arrays.fill(row, 0);
         }
-        echiquier[posXD][posYD] = 1;
+        chessboard[posXD][posYD] = 1;
 
-        /* Appel de la fonction récursive & affichage du nombre de solutions */
-        rechercheSolution(posXD, posYD, 2);
+        findSolutions(posXD, posYD, 2);
         System.out.println(nbSolutions);
     }
 }
